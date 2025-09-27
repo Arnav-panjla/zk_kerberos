72333d14c049328e845b23dd4f4dfd5c049e5971
Arnav-panjla <arnavpanjla@gmail.com>
2025-09-27 23:14:35 +0530

    modified app

--- zkk_app/risc0-circuit/methods/guest/src/main.rs
+++ zkk_app/risc0-circuit/methods/guest/src/main.rs
@@ -1,23 +1,50 @@
+#![no_main]
 use risc0_zkvm::guest::env;
+use sha2::{Sha256, Digest};
 
+#[no_mangle]
 fn main() {
 
     let input: Vec<u8> = env::read();
-
-    if input.len() < 21 {
-        eprintln!("Invalid input! Needs at least 21 bytes.");
+    
+    if input.len() < 29 {
+        eprintln!("Invalid input! Needs at least 29 bytes.");
         return;
     }
 
-    let user_id = String::from_utf8_lossy(&input[0..10]);
+    let userId = String::from_utf8_lossy(&input[0..10]);
 
-    let session_id = String::from_utf8_lossy(&input[11..21]);
+    let serviceId = String::from_utf8_lossy(&input[11..21]);
 
     let password = String::from_utf8_lossy(&input[22..]);
 
-    println!("UserID: {}", user_id);
-    println!("SessionID: {}", session_id);
-    println!("Password: {}", password);
+    let file_data = include_bytes!("../../../New Document.txt");
+    
+    let mut hasher = Sha256::new();
+    hasher.update(&userId);
+    hasher.update(&password);
+    hasher.update(&service_Id);
+    let ids_hash: [u8; 32] = hasher.finalize().into();
 
-    env::commit(&input);
+    let target_hash = hex::encode(&ids_hash);
+    let target_hash_hex = target_hash.as_bytes();
+    
+    let hash_exists = file_data
+        .windows(target_hash_hex.len())
+        .any(|window| window == target_hash_hex);
+
+    let hash_exists_bytes = if hash_exists { [1u8; 1] } else { [0u8; 1] };
+
+
+    let mut hasher = Sha256::new();
+    hasher.update(&file_data);
+    let file_data_hash: [u8; 32] = hasher.finalize().into();
+
+    let mut hasher = Sha256::new();
+    hasher.update(&userId);
+    hasher.update(&service_Id);
+    let session_hash: [u8; 32] = hasher.finalize().into();
+
+    env::commit(&(hash_exists_bytes, file_data_hash, session_hash));
+
 }
--- zkk_app/risc0-circuit/src/main.rs
+++ zkk_app/risc0-circuit/src/main.rs
@@ -1,34 +1,95 @@
-use ecdsa_methods::{ECDSA_VERIFY_ELF, ECDSA_VERIFY_ID};
-use risc0_zkvm::{ExecutorEnv, Receipt, default_prover};
-use log::{info, debug};
+
+mod keys;
+
+use methods::{RISC0_CIRCUIT_ELF, RISC0_CIRCUIT_ID};
+use risc0_zkvm::{default_prover, ExecutorEnv};
+use risc0_zkvm::Receipt;
+use rsa::Pkcs1v15Encrypt;
+use std::net::TcpStream;
+use bincode;
+use serde::{Serialize, Deserialize};
+
+
+#[derive(Debug, Serialize, Deserialize, bincode::Encode, bincode::Decode)]
+struct MessageReceived {
+    #[bincode(with_serde)]
+    u_pk: rsa::RsaPublicKey,
+    #[bincode(with_serde)]
+    proof: Receipt,
+}
+
+#[derive(Debug, bincode::Encode, bincode::Decode)]
+struct MessageSent {
+    signature: [u8; 64],
+    signature_data: SignBundle,
+}
+
+#[derive(Debug, bincode::Encode, bincode::Decode)]
+struct SignBundle {
+    ssk: String,
+    hash: [u8; 32],
+    timestamp: u64,
+}
+
 
 fn main() {
-    env_logger::init();
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .init();
 
-    let message = b"user123456 pass123456 asdfghjkl";
-    debug!("Initial input: {:?}", std::str::from_utf8(message).unwrap());
+    let (private_key, public_key) = keys::generate_rsa_keypair().expect("Failed to generate RSA key pair");
 
+    let user_id = b"1234567890";  
+    let password = b"password12"; 
+    let service_id = b"session456";  
+    
+    let mut input = [0u8; 30];
+    input[0..10].copy_from_slice(user_id);
+    input[10..20].copy_from_slice(password);
+    input[20..30].copy_from_slice(service_id);
+    
 
+    println!("{:?}", RISC0_CIRCUIT_ID);
+    let receipt = authenticate_user(input);
 
+    let m = MessageReceived{
+        u_pk: public_key,
+        proof: receipt,
+    };
+
+    let addr = "127.0.0.1:7878";
+    let mut stream = TcpStream::connect(addr).expect("failed to connect");
+    bincode::encode_into_std_write(m, &mut stream, bincode::config::standard()).expect("failed to serialize");
+    println!("Sent proof to {}", addr);
+
+    let res = bincode::decode_from_std_read::<Vec<u8>, _, _>(&mut stream, bincode::config::standard()).expect("failed to read/deserialize");
+
+    println!("Received response: {:?}", res);
+    if let Err(e) = stream.shutdown(std::net::Shutdown::Both) {
+        eprintln!("Failed to disconnect: {}", e);
+    } else {
+        println!("Disconnected from {}", addr);
+    }
+
+    let plaintext = private_key
+        .decrypt(Pkcs1v15Encrypt, &res)
+        .expect("Failed to decrypt response");
+    let (response, _): (MessageSent, _) =
+        bincode::decode_from_slice(&plaintext, bincode::config::standard())
+            .expect("Failed to deserialize decrypted response");
+    
+    println!("Decrypted response: {:?}", response);
+}
+
+pub fn authenticate_user(input: [u8; 30]) -> Receipt{
     let env = ExecutorEnv::builder()
-        .write(&message.to_vec())
-        .unwrap()
-        .build()
-        .unwrap();
+        .write(&input).unwrap()
+        .build().unwrap();
 
     let prover = default_prover();
+    let receipt = prover.prove(env, RISC0_CIRCUIT_ELF).unwrap().receipt;
 
-    let receipt = prover.prove(env, ECDSA_VERIFY_ELF).unwrap().receipt;
-    info!("zkVM execution completed, receipt generated");
-
-    debug!("Verifying receipt with method ID");
-    receipt.verify(ECDSA_VERIFY_ID).unwrap();
-    info!("Receipt verification successful");
-
-    let receipt_message: Vec<u8> = receipt.journal.decode().unwrap();
-    debug!("Journal decoded successfully");
-
-    debug!("Receipt message: {:?}", std::str::from_utf8(&receipt_message).unwrap());
-
-
+    receipt
 }
+
+
